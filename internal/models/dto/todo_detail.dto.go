96976fd86cce066bb8e348e5ab1cade47f23a4be
创圣大天使Evol <296662402@qq.com>
2024-06-01 21:27:26 +0800

    todo_detail.dto.go

---
+++ internal/models/dto/todo_detail.dto.go
@@ -0,0 +1,33 @@
+package dto
+
+import "github.com/crossevol/goadmin/internal/database/mysqlDao"
+
+type CreateTodoDetailDTO struct {
+	Desc   string `json:"desc"`
+	ImgUrl string `json:"img_url"`
+	TodoID int    `json:"todo_id"`
+}
+
+func NewCreateTodoDetailParams(dto *CreateTodoDetailDTO) *mysqlDao.CreateTodoDetailParams {
+	return &mysqlDao.CreateTodoDetailParams{
+		Desc:   dto.Desc,
+		ImgUrl: dto.ImgUrl,
+		TodoID: dto.TodoID,
+	}
+}
+
+type UpdateTodoDetailDTO struct {
+	Desc   string `json:"desc"`
+	ImgUrl string `json:"img_url"`
+	TodoID int    `json:"todo_id"`
+	ID     int    `json:"id"`
+}
+
+func NewUpdateTodoDetailParams(dto *UpdateTodoDetailDTO) *mysqlDao.UpdateTodoDetailParams {
+	return &mysqlDao.UpdateTodoDetailParams{
+		Desc:   dto.Desc,
+		ImgUrl: dto.ImgUrl,
+		TodoID: dto.TodoID,
+		ID:     dto.ID,
+	}
+}
