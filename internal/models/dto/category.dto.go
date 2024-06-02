e710009d956a2515402c4c30c72825ca19e9e22d
创圣大天使Evol <296662402@qq.com>
2024-06-02 16:20:27 +0800

    category.dto.go

---
+++ internal/models/dto/category.dto.go
@@ -0,0 +1,40 @@
+package dto
+
+import (
+	"database/sql"
+	"github.com/crossevol/goadmin/internal/database/mysqlDao"
+)
+
+type CreateCategoryDTO struct {
+	Name     string `db:"name" json:"name"`
+	ParentID int32  `db:"parent_id" json:"parent_id"`
+}
+
+type UpdateCategoryDTO struct {
+	Name     string `db:"name" json:"name"`
+	ParentID int32  `db:"parent_id" json:"parent_id"`
+}
+
+func NewCreateCategoryParmas(dto *CreateCategoryDTO) *mysqlDao.CreateCategoryParams {
+	params := &mysqlDao.CreateCategoryParams{
+		Name: dto.Name,
+	}
+	if dto.ParentID == 0 {
+		params.ParentID = sql.NullInt32{Int32: dto.ParentID, Valid: false}
+	} else {
+		params.ParentID = sql.NullInt32{Int32: dto.ParentID, Valid: true}
+	}
+	return params
+}
+
+func NewUpdateCategoryParmas(dto *CreateCategoryDTO) *mysqlDao.UpdateCategoryParams {
+	params := &mysqlDao.UpdateCategoryParams{
+		Name: dto.Name,
+	}
+	if dto.ParentID == 0 {
+		params.ParentID = sql.NullInt32{Int32: dto.ParentID, Valid: false}
+	} else {
+		params.ParentID = sql.NullInt32{Int32: dto.ParentID, Valid: true}
+	}
+	return params
+}
