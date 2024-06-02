2a523ae875c5469faae6b6af58690284080c82de
创圣大天使Evol <296662402@qq.com>
2024-06-02 15:36:34 +0800

    handle_todo_detail.go

--- cmd/api/handle_todo_detail.go
+++ cmd/api/handle_todo_detail.go
@@ -1,27 +1,138 @@
 package main
 
-import "net/http"
+import (
+	"github.com/crossevol/goadmin/internal/models/dto"
+	"github.com/crossevol/goadmin/internal/models/vo"
+	"github.com/crossevol/goadmin/internal/request"
+	"github.com/crossevol/goadmin/internal/response"
+	"github.com/crossevol/goadmin/internal/utils"
+	"net/http"
+	"strconv"
+	"strings"
+)
 
 func (app *application) GetTodoDetail(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is a protected handler"))
+	IdStr := r.PathValue("id")
+	id, err := strconv.ParseInt(IdStr, 10, 32)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+	todoDetail, err := app.q.GetTodoDetail(*app.ctx, int(id))
+	if err != nil {
+		app.notFound(w, r)
+		return
+	}
+	response.JSON(w, http.StatusOK, vo.NewTodoDetailVO(&todoDetail))
 }
 
 func (app *application) GetTodoDetailList(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is a protected handler"))
+	query := r.URL.Query()
+	hasIds := query.Has("id")
+	if hasIds {
+		app.GetTodoDetailByIDs(w, r)
+		return
+	}
+	app.logger.Info(utils.ToJson(query))
+	todoDetails, err := app.q.GetTodoDetails(*app.ctx)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	response.JSON(w, http.StatusOK, vo.NewTodoDetailVOs(todoDetails))
 }
 
 func (app *application) GetTodoDetailByIDs(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is a protected handler"))
+	query := r.URL.Query()
+	idsStr := query.Get("id")
+	idsArr := strings.Split(idsStr, ",")
+	var ids []int
+	for _, s := range idsArr {
+		id, err := strconv.ParseInt(s, 10, 32)
+		if err != nil {
+			app.badRequest(w, r, err)
+			return
+		}
+		ids = append(ids, int(id))
+	}
+	todoDetailsByIDs, err := app.q.GetTodoDetailsByIDs(*app.ctx, ids)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	response.JSON(w, http.StatusOK, vo.NewTodoDetailVOs(todoDetailsByIDs))
 }
 
 func (app *application) CreateTodoDetail(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is a protected handler"))
+	createTodoDetailDTO := dto.CreateTodoDetailDTO{}
+	err := request.DecodeJSON(w, r, &createTodoDetailDTO)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+	createTodoDetailParams := dto.NewCreateTodoDetailParams(&createTodoDetailDTO)
+	result, err := app.q.CreateTodoDetail(*app.ctx, *createTodoDetailParams)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	lastInsertId, err := result.LastInsertId()
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	todoDetail, err := app.q.GetTodoDetail(*app.ctx, int(lastInsertId))
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	response.JSON(w, http.StatusOK, vo.NewTodoDetailVO(&todoDetail))
 }
 
 func (app *application) UpdateTodoDetail(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is a protected handler"))
+	IdStr := r.PathValue("id")
+	id, err := strconv.ParseInt(IdStr, 10, 32)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+	updateTodoDetailDTO := dto.UpdateTodoDetailDTO{}
+	err = request.DecodeJSON(w, r, &updateTodoDetailDTO)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+	updateTodoDetailParams := dto.NewUpdateTodoDetailParams(&updateTodoDetailDTO)
+	updateTodoDetailParams.ID = int(id)
+	_, err = app.q.UpdateTodoDetail(*app.ctx, *updateTodoDetailParams)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	todoDetail, err := app.q.GetTodoDetail(*app.ctx, updateTodoDetailParams.ID)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	response.JSON(w, http.StatusOK, vo.NewTodoDetailVO(&todoDetail))
 }
 
 func (app *application) DeleteTodoDetail(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is a protected handler"))
+	IdStr := r.PathValue("id")
+	id, err := strconv.ParseInt(IdStr, 10, 32)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+	todoDetail, err := app.q.GetTodoDetail(*app.ctx, int(id))
+	if err != nil {
+		app.notFound(w, r)
+		return
+	}
+	err = app.q.DeleteTodoDetail(*app.ctx, int(id))
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	response.JSON(w, http.StatusOK, vo.NewTodoDetailVO(&todoDetail))
 }
